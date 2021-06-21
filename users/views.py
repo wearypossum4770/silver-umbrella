31b0a77db9b9a3d02ec316936f0ef60e43394a19
Stephen Smith <wearypossum4770@yahoo.com>
2021-06-20 22:31:20 -0400

    sync

--- users/views.py
+++ users/views.py
@@ -22,39 +22,46 @@ def about(request):
     return render(request, "users/about.html")
 
 
-def registration(request):
-    form = UserRegisterForm
-    if request.method == "POST":
-        form = (request.POST)
+
+
+def register(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
         if form.is_valid():
             form.save()
-            username = form.cleaned_data.get("username")
-            messages.success(
-                request,
-                f" {username} Your account has been created! You are now able to log in",
-            )
-            return redirect("login")
-    form()
-    return render(request, "users/register.html", {"form": form})
-
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Your account has been created! You are now able to log in')
+            return redirect('login')
+    else:
+        form = UserRegisterForm()
+    return render(request, 'users/register.html', {'form': form})
 
 def handle_addresses(user, *args, **kwargs):
     addr = Profile.objects.get(user=user)
     print(user)
     print(addr)
 
-
 @login_required
 def profile(request):
-    data = request.POST
-    user = request.user
-    handle_addresses(request.user)
-    user_form = UserUpdateForm(data, instance=user)
-    profile_form = ProfileUpdateForm(data, request.FILES, instance=user.profile)
-    if request.method == "POST":
+
+    if request.method == 'POST':
+        user_form = UserUpdateForm(request.POST, instance=request.user)
+        profile_form = ProfileUpdateForm(request.POST,
+                                   request.FILES,
+                                   instance=request.user.profile)
         if user_form.is_valid() and profile_form.is_valid():
-            user_form.save() and profile_form.save()
-            messages.success(request, f"Your account has been updated!")
-            return redirect("profile")
-    context = {"user_form": user_form, "profile_form": profile_form}
-    return render(request, "users/profile.html", context)
+            user_form.save()
+            profile_form.save()
+            messages.success(request, f'Your account has been updated!')
+            return redirect('profile')
+
+    else:
+        user_form = UserUpdateForm(instance=request.user)
+        profile_form = ProfileUpdateForm(instance=request.user.profile)
+
+    context = {
+        'user_form': user_form,
+        'profile_form': profile_form
+    }
+
+    return render(request, 'users/profile.html', context)
