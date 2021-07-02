38bc652a34aee0ee02c8fa287e6616926ee1bf58
stephen smith <wearypossum4770@yahoo.com>
2021-07-02 14:34:56 -0400

    sync

--- tasks/tests.py
+++ tasks/tests.py
@@ -12,28 +12,38 @@ from tasks.models import Task
 class TestTasks(TestCase):
     fixtures = ("datainit.json",)
     pytestmark = pytest.mark.django_db
+
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
         cls.task_10 = Task.objects.get(pk=10)
         cls.tasks = Task.objects.all()
+
     @classmethod
     def tearDownClass(cls):
         super().tearDownClass()
+
     def test_task_list_loaded(self):
         assert len(self.tasks) > 0
-    def test_task_10_title (self):
+
+    def test_task_10_title(self):
         assert self.task_10.title == "ITM304"
+
     def test_task_10_content(self):
-        assert self.task_10.content ==  "Quiz 6 SQL, Database Redesign"
+        assert self.task_10.content == "Quiz 6 SQL, Database Redesign"
+
     def test_task_10_date_modified(self):
         assert self.task_10.date_modified == None
+
     def test_task_10_date_created(self):
         assert self.task_10.date_created == self.task_10.date_created
+
     def test_task_10_date_due(self):
         assert self.task_10.date_due == None
+
     def test_task_10_completed(self):
         assert self.task_10.completed == True
+
 
 # [
 # {"task":"HTML I","done":true},
